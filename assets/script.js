1c6e031c2960d5397f214ab0f64b5e4f44597989
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-07-15 10:05:01 -0400

    huge refactor, html, css, and JS

--- assets/script.js
+++ assets/script.js
@@ -51,65 +51,12 @@ function showConditions(city) {
       });
 
       // date and city name in dashboard container
-      $("#weather-dashboard").html(
-          `${city} ${day} <img src='http://openweathermap.org/img/w/${icon}.png'/>`
-      );
-      // weather conditions
-      $("#temp").html(`Temperature: ${data.current.temp} F`);
-      $("#wind").html(`Wind Speed: ${data.current.wind_speed} mph`);
-      $("#humidity").html(`Humidity: ${data.current.humidity} %`);
-      // reset
-      $(".5day-forecast").html("");
+      
 
-      // loop data through 5 cards
-      for (let i = 1; i < 6; i++) {
-          icon = data.daily[i].weather[0].icon;
-          eachDay = new Date(data.daily[i].dt * 1000).toLocaleDateString("en-US", {
-              timeZone: `${data.timeZone}`,
-            });
-          // fill forecast cards
-          $(".5day-forecast").append(`
-          <div class="col-auto mb-3">
-          <div class="card text-light cardbg no-gutters" style="width: 14rem;">
-              <div class="card-body">
-                  <h5 class="card-title">${day}</h5>
-                  <p class="card-text my-3"><img src='http://openweathermap.org/img/w/${icon}.png'/></p>
-                  <p class="card-text my-3">Temperature: ${data.daily[i].temp.day} F</p>
-                  <p class="card-text my-3">Wind Speed: ${data.daily[i].wind_speed} mph</p>
-                  <p class="card-text my-3">Humidity: ${data.daily[i].humidity} %</p>                
-              </div>
-          </div>`);
-      }
-  })
-}
-
-// a function to control the search bar
-function searchBar(event) {
-  event.preventDefault();
-  // set entry as variable
-  city = $("#city").val();
-  // if entry was already searched, display its data
-  if (searchHistory.includes(city)) {
-      displayConditions(city);
-  } else {
-      // store search history, display the returned weather conditions
-      searchHistory.unshift(city);
-      localStorage.setItem("searches", JSON.stringify(searchHistory));
-      displayConditions(city);
-  }
-
-}
+      // loop data through weather forecast cards
 
 
 
-// see if I can make this work???
-// // a function to display the searches saved to localstorage
-// function displaySearchHistory() {
-//   searchHistory = JSON.parse(localStorage.getItem("searches"));
-//   console.log(searchHistory);
-//   if (searchHistory == null) searchHistory = [];
-//   // only display 5 most recent searches
-//   if (searchHistory.length > 5) {
-//     searchHistory.pop();
-//   }
-// }
+
+
+
